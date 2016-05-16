4bb5e02251f1e4cbeac355f25f1167172560c2de
Kamil <kamilkryjak95@gmail.com>
2016-05-16 16:57:37 +0200

    Add list of possible moves for normal pawns

--- checkers.hs
+++ checkers.hs
@@ -2,8 +2,19 @@ import Data.List.Split
 initialBoardStr = ".b.b.b.b\nb.b.b.b.\n.b.b.b.b\n........\n........\nw.w.w.w.\n.w.w.w.w\nw.w.w.w."
 
 genPos rows = [(c, d) | c <- [1..8], d <- rows, or [and [even c, even d], and [odd c, odd d]] ]
+
+directions = [(1,1), (-1,1), (-1,-1), (1,-1)]
+
+tupSum (x,y) (x1,y1) = (x+x1, y+y1)
+
+size :: Int
+size = 8
+
+onBoard (x,y)
+	|	(x > 0) && (x <=size) && (y > 0) && (y<=size) = True
+	|	otherwise = False
 				
-data Pawn = White | Blue | QWhite | QBlue | Empty
+data Pawn = White | Blue | QWhite | QBlue | Empty deriving Eq
 
 initialBoard = [[Empty,Blue,Empty,Blue,Empty,Blue,Empty,Blue],
 				[Blue,Empty,Blue,Empty,Blue,Empty,Blue,Empty],
@@ -11,6 +22,15 @@ initialBoard = [[Empty,Blue,Empty,Blue,Empty,Blue,Empty,Blue],
 				[Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty],
 				[Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty],
 				[White,Empty,White,Empty,White,Empty,White,Empty],
+				[Empty,White,Empty,White,Empty,White,Empty,White],
+				[White,Empty,White,Empty,White,Empty,White,Empty]]
+
+testingBoard = [[Empty,Blue,Empty,Blue,Empty,Blue,Empty,Blue],
+				[Blue,Empty,Blue,Empty,Blue,Empty,Blue,Empty],
+				[Empty,Blue,Empty,Blue,Empty,Blue,Empty,Blue],
+				[Empty,Empty,White,Empty,Empty,Empty,Empty,Empty],
+				[Empty,Empty,Empty,Empty,Empty,White,Empty,Empty],
+				[White,Empty,Empty,Empty,Blue,Empty,White,Empty],
 				[Empty,White,Empty,White,Empty,White,Empty,White],
 				[White,Empty,White,Empty,White,Empty,White,Empty]]
 
@@ -23,9 +43,6 @@ instance Show Pawn where
 
 data Board = Board [[Pawn]] deriving Show
 
-size :: Int
-size = 8
-
 printRow [] = ""
 printRow (x:xs) = show x ++ "" ++ printRow xs
 
@@ -34,7 +51,6 @@ printBoard (x:xs) = printRow x ++ "\n" ++ printBoard xs
 
 parseStr "" = []
 parseStr (x:xs) = x : parseStr xs
-
 
 initRow [] = []
 initRow (x:xs) 
@@ -59,4 +75,17 @@ removePos (x,y) board = setPos (x,y) Empty board
 getPawn (x,y) board = (board !! (y-1)) !! (x-1)
 
 move (x,y) (x1, y1) board = setPos (x1,y1) (getPawn (x,y) board) (setPos(x,y) Empty board) 
+
+checkPossiblePos [] [] _ _ _ = []
+checkPossiblePos (p:ps) (d:ds) z pawn board 
+	|	((snd d) == z) && (onBoard p) && (getPawn p board == Empty) =  p :checkPossiblePos ps ds z pawn board
+	|	(onBoard p) && ((getPawn p board == White) || (getPawn p board == QWhite)) && (onBoard (tupSum p d)) && (getPawn (tupSum p d) board == Empty) = (tupSum p d) : checkPossiblePos ps ds z pawn board
+	|	(onBoard p) && ((getPawn p board == Blue) || (getPawn p board == QBlue)) && (onBoard (tupSum p d)) && (getPawn (tupSum p d) board == Empty) = (tupSum p d) : checkPossiblePos ps ds z pawn board
+	| 	otherwise = checkPossiblePos ps ds z pawn board
+
+findPossiblePos (x,y) pawn@Blue board =  checkPossiblePos (map (tupSum (x,y)) directions) directions 1 pawn board 
+
+findPossiblePos (x,y) pawn@White board =  checkPossiblePos (map (tupSum (x,y)) directions) directions (-1) pawn board 
+
+possiblePos (x,y) board = findPossiblePos (x,y) (getPawn (x,y) board) board
 	
